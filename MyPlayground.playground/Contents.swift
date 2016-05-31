0ed25c6a068185e5fdcfd0f9a4b2fd85b0f115b1
Amandeep Chawla <amandeep@adobe.com>
2016-06-01 00:11:23 +0530

    Basic Playground

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,23 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var str2:String? = nil              // adding optionals (?) to allow them to be set to nil
+
+// var str2nil:String = nil         // it is not allowed
+
+var str3:String = "Hello Aman!"
+
+// let allows constants
+let constantString = "Hello"
+
+// constantString = "Hello Aman" not allowed as let is constant
+
+print(constantString)
+
+
+
+
+
