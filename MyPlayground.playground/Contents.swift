3c645f3801dbf950daa12538800fc1a090d84b11
Amandeep Chawla <amandeep@adobe.com>
2016-06-01 12:30:22 +0530

    for in to enumerate dictionaries

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -247,6 +247,24 @@ switch vegetable {
     
 }
 
+let interestingNumbers = [
+    "Prime": [2, 3, 5, 7, 11, 13],
+    "Fibonacci": [1, 1, 2, 3, 5, 8],
+    "Square": [1, 4, 9, 16, 25],
+]
+var largest = 0
+var largestKind:String = ""
+
+for (kind, numbers) in interestingNumbers {
+    for number in numbers {
+        if number > largest {
+            largest = number
+            largestKind = kind
+        }
+    }
+}
+print(largest)
+print(largestKind)
 
 
 
