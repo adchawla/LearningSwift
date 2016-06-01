ad4ebf8cd4fcd7afa4dfa34d129c0dc019776eb0
Amandeep Chawla <amandeep@adobe.com>
2016-06-01 12:02:32 +0530

    if let example

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -210,6 +210,23 @@ shoppingList
 occupations
 
 
+// if let
+
+
+var optionalString:String? = "Hello"
+
+print(optionalString == nil)
+
+var optionalName:String? = "John Appleseed"
+
+var greeting = "Hello!"
+
+if let name = optionalName {
+    greeting = "Hello, \(name)"
+} else {
+    greeting = "Hello, unknown"
+}
+
 
 
 
